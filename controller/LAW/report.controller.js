9de6f3fe070489a28c1c2b4c3bcc8b33a5ea8c2c
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2024-03-11 12:14:09 +0100

    It's a new week

--- controller/LAW/report.controller.js
+++ controller/LAW/report.controller.js
@@ -1,12 +1,21 @@
+const { caseCodeEmailSender } = require("../../middleware/LAW/emailSender.utils");
+const { caseCodeFinder } = require("../../utils/LAW/abuseDBRelator");
 const { gettingReportLength, caseCodeCreator, reverseString } = require("../../utils/LAW/abuseReport.utils");
 const { emailValidator } = require("../../utils/validator");
 
 
 const lawReportPoster = async (req, res) => {
-    const { name, age, gender, abuse, evidence, evidenceType, actionTaken, actionWants, email, phone } = req.body;
+    const { name, age, gender, abuse, evidence, evidenceType, actionTaken, actionWant, email, phone, reportMeans, who } = req.body;
     try {
 
-        if(!name || !age || !gender || !abuse || !evidence || !actionTaken || !actionWants || !email || !phone ) return res.status(402).json({error: 'All questions must be answered', success: false})
+        let caseCode ;
+        let receivedCode
+
+        // ============================ TODO ============================= //
+        // VALIDATE ALL USER INPUT TO DETECT PROXY
+        // SEND CASECODE TO USER EMAIL 
+
+        if( !name || !age || !gender || !abuse || !evidence || !actionTaken || !actionWant || !email || !phone || !reportMeans || !who ) return res.status(402).json({error: 'All questions must be answered', success: false})
 
         // ===================== VALIDATING EMAIL ========================= //
         const isEmail = emailValidator(email);
@@ -19,11 +28,41 @@ const lawReportPoster = async (req, res) => {
         
         // ==================== CREATING CASE CODE ==================== //
         // ============== REVERSING ABUSE TYPE BEFORE USING IT =============== //
-        const reverser = await reverseString(abuse)
-        const caseCode = await caseCodeCreator(reverser)
+        const reverser = reverseString(abuse)
+        receivedCode = caseCodeCreator(reverser)
 
-        console.log('case id', caseId)
-        console.log('case code', caseCode)
+        const isExistCode = await caseCodeFinder(receivedCode)
+
+        // ============== CHECKING IF CASECODE EXISTS IN DATABASE ============== //
+        if(isExistCode){
+            receivedCode = caseCodeCreator(reverser)
+        } else {
+            caseCode = receivedCode
+        }
+
+        const storeReport = {
+            name,
+            email,
+            phone,
+            age,
+            gender,
+            abuse,
+            evidence,
+            evidenceType,
+            actionTaken,
+            actionWant,
+            caseId,
+            caseCode,
+            caseStatus: 'pending',
+            reportMeans,
+            who
+        }
+
+        console.log(storeReport)
+
+        const sendEmail = await caseCodeEmailSender(email, name, caseCode)
+
+        res.status(201).json({success: true, message: 'Your Report has been received and will be looked into, kindly check your email for your case tracking code', caseCode: caseCode })
         
     } catch (err) {
         res.status(401).json({error: 'something went wrong check the error log or try again later', success: false, errLog: err});
